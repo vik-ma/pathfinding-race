267968e6fc17584dd0574240a42e3b7555b73762
Viktor <vikt.mag@gmail.com>
2022-08-31 00:06:20 +0200

    Added pathToGoal to BidirectionalSearch

--- src/Algorithms/BidirectionalSearch.js
+++ src/Algorithms/BidirectionalSearch.js
@@ -1,29 +1,50 @@
 //Bidirectional Breadth First Search
 export function BidirectionalSearch(startNode, goalNode) {
+  var path = [];
+  var pathFoundMessage = "CANT FIND PATH";
+  var pathToGoal = [];
+
   var sourceQueue = [];
   var destinationQueue = [];
-  var path = [];
   sourceQueue.push(startNode);
   destinationQueue.push(goalNode);
-  var pathFoundMessage = "CANT FIND PATH";
+
 
   while (sourceQueue.length !== 0 || destinationQueue.length !== 0) {
     if (sourceQueue.length !== 0) {
       let currentNodeFwd = sourceQueue.shift();
       path.push(currentNodeFwd);
       let adjacentNodesFwd = currentNodeFwd.adjacentNodes;
-      console.log(`VISITED ${currentNodeFwd.row} ${currentNodeFwd.col}`);
+      // console.log(`VISITED ${currentNodeFwd.row} ${currentNodeFwd.col}`);
       currentNodeFwd.isVisited = true;
 
       if (currentNodeFwd.isVisited && currentNodeFwd.isVisitedBidirectional) {
-        pathFoundMessage = `PATH FOUND - INTERSECTION: ${currentNodeFwd.row} ${currentNodeFwd.col}`;
-        return { path, pathFoundMessage };
+        pathFoundMessage = `PATH FOUND - INTERSECTION (FORWARD): ${currentNodeFwd.row} ${currentNodeFwd.col}`;
+
+        let tempNodeFwd = currentNodeFwd;
+        pathToGoal.push(tempNodeFwd);
+        while (tempNodeFwd.previousNode) {
+          pathToGoal.push(tempNodeFwd.previousNode);
+          tempNodeFwd = tempNodeFwd.previousNode;
+        }
+  
+        pathToGoal = pathToGoal.reverse();
+
+        let tempNodeBack = currentNodeFwd;
+        while (tempNodeBack.previousNodeBidirectional) {
+          pathToGoal.push(tempNodeBack.previousNodeBidirectional);
+          tempNodeBack = tempNodeBack.previousNodeBidirectional;
+        }
+
+        return { path, pathFoundMessage, pathToGoal };
+
       }
 
       for (let i = 0; i < adjacentNodesFwd.length; i++) {
         if (!adjacentNodesFwd[i].isVisited) {
           sourceQueue.push(adjacentNodesFwd[i]);
           adjacentNodesFwd[i].isVisited = true;
+          adjacentNodesFwd[i].previousNode = currentNodeFwd;
         }
       }
     }
@@ -31,18 +52,37 @@ export function BidirectionalSearch(startNode, goalNode) {
       let currentNodeBack = destinationQueue.shift();
       path.push(currentNodeBack);
       let adjacentNodesBack = currentNodeBack.adjacentNodes;
-      console.log(`VISITED ${currentNodeBack.row} ${currentNodeBack.col}`);
+      // console.log(`VISITED ${currentNodeBack.row} ${currentNodeBack.col}`);
       currentNodeBack.isVisitedBidirectional = true;
 
       if (currentNodeBack.isVisited && currentNodeBack.isVisitedBidirectional) {
-        pathFoundMessage = `PATH FOUND - INTERSECTION: ${currentNodeBack.row} ${currentNodeBack.col}`;
-        return { path, pathFoundMessage };
+        pathFoundMessage = `PATH FOUND - INTERSECTION (BACK): ${currentNodeBack.row} ${currentNodeBack.col}`;
+
+        let tempNodeBack = currentNodeBack;
+        pathToGoal.push(tempNodeBack);
+        while (tempNodeBack.previousNodeBidirectional) {
+          pathToGoal.push(tempNodeBack.previousNodeBidirectional);
+          tempNodeBack = tempNodeBack.previousNodeBidirectional;
+        }
+  
+        pathToGoal = pathToGoal.reverse();
+
+        let tempNodeFwd = currentNodeBack;
+        while (tempNodeFwd.previousNode) {
+          pathToGoal.push(tempNodeFwd.previousNode);
+          tempNodeFwd = tempNodeFwd.previousNode;
+        }
+
+        pathToGoal = pathToGoal.reverse();
+        
+        return { path, pathFoundMessage, pathToGoal };
       }
 
       for (let i = 0; i < adjacentNodesBack.length; i++) {
         if (!adjacentNodesBack[i].isVisitedBidirectional) {
           destinationQueue.push(adjacentNodesBack[i]);
           adjacentNodesBack[i].isVisitedBidirectional = true;
+          adjacentNodesBack[i].previousNodeBidirectional = currentNodeBack;
         }
       }
     }
