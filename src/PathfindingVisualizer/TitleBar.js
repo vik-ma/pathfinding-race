020e7a74c02848ec5a84ec090a679071c863de2b
Viktor <vikt.mag@gmail.com>
2022-09-08 00:14:10 +0200

    Added styling for selected algorithm in TitleBar

--- src/PathfindingVisualizer/TitleBar.js
+++ src/PathfindingVisualizer/TitleBar.js
@@ -2,10 +2,21 @@ import React, { useState, useEffect, useContext } from "react";
 import { GridContext } from "../Helpers/GridContexts";
 
 export const TitleBar = () => {
-  const { algoList, styles, algorithmMap, numStartNodes, nodeDivMap, userChoice, setUserChoice } =
-    useContext(GridContext);
+  const {
+    algoList,
+    styles,
+    algorithmMap,
+    numStartNodes,
+    nodeDivMap,
+    userChoice,
+    setUserChoice,
+  } = useContext(GridContext);
 
+  const [selectedDiv, setSelectedDiv] = useState("");
 
+  const setChoice = (num) => {
+    setUserChoice(num);
+  };
 
   return (
     <div>
@@ -15,27 +26,42 @@ export const TitleBar = () => {
           <div className={nodeDivMap["nodegoal"]}>G</div> FIRST?
         </div>
         <div
-          className="titleBarChoices"
+          className={`titleBarChoices ${
+            selectedDiv === "algoChoice1" ? " selectedChoice" : undefined
+          }`}
+          onClick={() => {
+            setSelectedDiv("algoChoice1");
+            setUserChoice(1);
+          }}
           id="algoChoice1"
-          onClick={() => setUserChoice(1)}
         >
           <div className={nodeDivMap["nodestart1"]}>1</div>{" "}
           {algorithmMap[algoList[0]]}
         </div>
 
         <div
-          className="titleBarChoices"
+          className={`titleBarChoices ${
+            selectedDiv === "algoChoice2" ? " selectedChoice" : undefined
+          }`}
+          onClick={() => {
+            setSelectedDiv("algoChoice2");
+            setUserChoice(2);
+          }}
           id="algoChoice2"
-          onClick={() => setUserChoice(2)}
         >
           <div className={nodeDivMap["nodestart2"]}>2</div>{" "}
           {algorithmMap[algoList[1]]}
         </div>
         {numStartNodes > 2 ? (
           <div
-            className="titleBarChoices"
+            className={`titleBarChoices ${
+              selectedDiv === "algoChoice3" ? " selectedChoice" : undefined
+            }`}
+            onClick={() => {
+              setSelectedDiv("algoChoice3");
+              setUserChoice(3);
+            }}
             id="algoChoice3"
-            onClick={() => setUserChoice(3)}
           >
             <div className={nodeDivMap["nodestart3"]}>3</div>{" "}
             {algorithmMap[algoList[2]]}
@@ -43,22 +69,32 @@ export const TitleBar = () => {
         ) : null}
         {numStartNodes > 3 ? (
           <div
-            className="titleBarChoices"
+            className={`titleBarChoices ${
+              selectedDiv === "algoChoice4" ? " selectedChoice" : undefined
+            }`}
+            onClick={() => {
+              setSelectedDiv("algoChoice4");
+              setUserChoice(4);
+            }}
             id="algoChoice4"
-            onClick={() => setUserChoice(4)}
           >
             <div className={nodeDivMap["nodestart4"]}>4</div>{" "}
             {algorithmMap[algoList[3]]}
           </div>
         ) : null}
         <div
-          className="titleBarChoices"
+          className={`titleBarChoices ${
+            selectedDiv === "algoChoice5" ? " selectedChoice" : undefined
+          }`}
+          onClick={() => {
+            setSelectedDiv("algoChoice5");
+            setUserChoice(5);
+          }}
           id="algoChoice5"
-          onClick={() => setUserChoice(5)}
         >
           <div className={nodeDivMap["nodex"]}>X</div> No Path Will Be Found!
         </div>
-        <div>{userChoice}</div>
+        <div></div>
       </div>
     </div>
   );
