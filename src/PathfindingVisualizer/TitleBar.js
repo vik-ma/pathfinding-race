e669c180d138c4905f557e2efc9e944d9b465cac
Viktor <vikt.mag@gmail.com>
2022-09-08 04:30:15 +0200

    Added proper messages for path lengths in TitleBar

--- src/PathfindingVisualizer/TitleBar.js
+++ src/PathfindingVisualizer/TitleBar.js
@@ -37,7 +37,15 @@ export const TitleBar = () => {
             <div className={nodeDivMap["nodestart1"]}>1</div>{" "}
             {algorithmMap[algoList[0]]}
           </div>
-          <div className="algoPathMsg">{renderWinnerMsg && !winnerAlgo[5] ? `${calculatedAlgoMap[1].path.length}` : null}</div>
+          <div className="algoPathMsg">
+            {renderWinnerMsg
+              ? calculatedAlgoMap[1].pathIsFound
+                ? `Found the goal node after visiting ${
+                    calculatedAlgoMap[1].path.length - 1
+                  } nodes!`
+                : "Could not find the goal node!"
+              : null}
+          </div>
         </div>
         <div>
           <div
@@ -53,7 +61,15 @@ export const TitleBar = () => {
             <div className={nodeDivMap["nodestart2"]}>2</div>{" "}
             {algorithmMap[algoList[1]]}
           </div>
-          <div className="algoPathMsg">{renderWinnerMsg && !winnerAlgo[5] ? `${calculatedAlgoMap[2].path.length}` : null}</div>
+          <div className="algoPathMsg">
+            {renderWinnerMsg
+              ? calculatedAlgoMap[2].pathIsFound
+                ? `Found the goal node after visiting ${
+                    calculatedAlgoMap[2].path.length - 1
+                  } nodes!`
+                : "Could not find the goal node!"
+              : null}
+          </div>
         </div>
         {numStartNodes > 2 ? (
           <div>
@@ -70,7 +86,15 @@ export const TitleBar = () => {
               <div className={nodeDivMap["nodestart3"]}>3</div>{" "}
               {algorithmMap[algoList[2]]}
             </div>
-            <div className="algoPathMsg">{renderWinnerMsg && !winnerAlgo[5] ? `${calculatedAlgoMap[3].path.length}` : null}</div>
+            <div className="algoPathMsg">
+              {renderWinnerMsg
+                ? calculatedAlgoMap[3].pathIsFound
+                  ? `Found the goal node after visiting ${
+                      calculatedAlgoMap[3].path.length - 1
+                    } nodes!`
+                  : "Could not find the goal node!"
+                : null}
+            </div>
           </div>
         ) : null}
         {numStartNodes > 3 ? (
@@ -88,7 +112,15 @@ export const TitleBar = () => {
               <div className={nodeDivMap["nodestart4"]}>4</div>{" "}
               {algorithmMap[algoList[3]]}
             </div>
-            <div className="algoPathMsg">{renderWinnerMsg && !winnerAlgo[5] ? `${calculatedAlgoMap[4].path.length}` : null}</div>
+            <div className="algoPathMsg">
+              {renderWinnerMsg
+                ? calculatedAlgoMap[4].pathIsFound
+                  ? `Found the goal node after visiting ${
+                      calculatedAlgoMap[4].path.length - 1
+                    } nodes!`
+                  : "Could not find the goal node!"
+                : null}
+            </div>
           </div>
         ) : null}
         <div>
@@ -104,7 +136,11 @@ export const TitleBar = () => {
           >
             <div className={nodeDivMap["nodex"]}>X</div> No Path Will Be Found!
           </div>
-          <div className="algoPathMsg">{renderWinnerMsg && winnerAlgo[5] ? "No algorithms were able to reach the goal node!" : null}</div>
+          <div className="algoPathMsg">
+            {renderWinnerMsg && winnerAlgo[5]
+              ? "No algorithms were able to find the goal node!"
+              : null}
+          </div>
         </div>
       </div>
     </div>
