24a01f792fef31b2ee0bc0e98b535a112e1133ff
Viktor <vikt.mag@gmail.com>
2022-09-18 13:31:51 +0200

    Added dimensions for infoBackdrop

--- src/InfoSliders/SliderWindow.js
+++ src/InfoSliders/SliderWindow.js
@@ -5,7 +5,7 @@ export const SliderWindow = () => {
   const { setIsInfoRendered } = useContext(GridContext);
   return (
     <div className="popupBackground">
-      <div className="settingsBackdrop">
+      <div className="settingsBackdrop infoBackdrop">
         TEST<button onClick={() => setIsInfoRendered(false)}>BACK</button>
       </div>
     </div>
