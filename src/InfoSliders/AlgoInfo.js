f72b051d724e9a5fb27b25b55bbc43060b62ce88
Viktor <vikt.mag@gmail.com>
2022-09-19 22:41:49 +0200

    Added all algorithms to AlgoInfo.js

--- src/InfoSliders/AlgoInfo.js
+++ src/InfoSliders/AlgoInfo.js
@@ -1,17 +1,94 @@
 export const AlgoInfo = [
   {
-    id: "DEFAULT",
+    id: "SLIDER-DEFAULT",
     title: <h1>Information</h1>,
-    text: <>AAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA </>,
+    text: (
+      <>
+        DEFAULT DEFAULT DEFAULT DEFAULT DEFAULT DEFAULT DEFAULT DEFAULT DEFAULT
+        DEFAULT DEFAULT DEFAULT DEFAULT DEFAULT DEFAULT DEFAULT DEFAULT DEFAULT
+        DEFAULT DEFAULT DEFAULT DEFAULT DEFAULT
+      </>
+    ),
   },
   {
-    id: "BFS",
+    id: "SLIDER-BFS",
     title: <h1>Breadth First Search</h1>,
-    text: <>AAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA dsfdsffdsfdsfdsAAAAA AAAAAAAAAAAAAAAAAAAAA AAAsssAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAafafafAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA AafafaffAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAA AAAAyyasysAA AAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA </>,
+    text: (
+      <>
+        Breadth First SearchBreadth First SearchBreadth First Search Breadth
+        First SearchBreadth First SearchBreadth First SearchBreadth First Search
+        Breadth First SearchBreadth First SearchBreadth First SearchBreadth
+        First SearchBreadth First SearchBreadth First SearchBreadth First
+        SearchBreadth First Search Breadth First Search
+      </>
+    ),
   },
   {
-    id: "DFS",
+    id: "SLIDER-DFS",
     title: <h1>Depth First Search</h1>,
-    text: <>DDDDDDDDDdddd</>,
+    text: (
+      <>
+        Depth First Search Depth First Search Depth First Search Depth First
+        SearchDepth First SearchDepth First SearchDepth First Search Depth First
+        Search Depth First SearchDepth First SearchDepth First Search Depth
+        First SearchDepth First SearchvvDepth First Searchvv
+      </>
+    ),
+  },
+  {
+    id: "SLIDER-DIJKSTRA",
+    title: <h1>Dijkstra's Algorithm</h1>,
+    text: (
+      <>
+        Dijkstra's Algoritm Dijkstra's Algoritm Dijkstra's Algoritm Dijkstra's
+        AlgoritmDijkstra's AlgoritmDijkstra's AlgoritmDijkstra's
+        AlgoritmDijkstra's AlgoritmDijkstra's Algoritm Dijkstra's Algoritm
+        Dijkstra's Algoritm Dijkstra's Algoritm v Dijkstra's AlgoritmDijkstra's
+        AlgoritmDijkstra's Algoritm
+      </>
+    ),
+  },
+  {
+    id: "SLIDER-ASTAR",
+    title: <h1>A-Star Search</h1>,
+    text: (
+      <>
+        A-Star SearchA-Star SearchA-Star SearchA-Star SearchA-Star SearchA-Star
+        SearchA-Star SearchA-Star SearchA-Star SearchA-Star Search A-Star
+        SearchA-Star SearchA-Star SearchA-Star Search A-Star SearchA-Star
+        SearchA-Star SearchA-Star SearchA-Star Search A-Star Search A-Star
+        Search
+      </>
+    ),
+  },
+  {
+    id: "SLIDER-GBFS",
+    title: <h1>Greedy Best-First Search</h1>,
+    text: (
+      <>
+        Greedy Best-First SearchGreedy Best-First SearchGreedy Best-First
+        SearchGreedy Best-First Search Greedy Best-First Search Greedy
+        Best-First Search Greedy Best-First SearchGreedy Best-First SearchGreedy
+        Best-First SearchGreedy Best-First SearchGreedy Best-First SearchGreedy
+        Best-First SearchGreedy Best-First SearchGreedy Best-First Search
+      </>
+    ),
+  },
+  {
+    id: "SLIDER-BIDIR",
+    title: <h1>Bidirectional Search</h1>,
+    text: (
+      <>
+        Bidirectional SearchBidirectional SearchBidirectional
+        SearchBidirectional SearchBidirectional SearchBidirectional
+        SearchBidirectional SearchBidirectional SearchBidirectional
+        SearchBidirectional SearchBidirectional SearchBidirectional
+        SearchBidirectional SearchBidirectional SearchBidirectional
+        SearchBidirectional SearchBidirectional SearchBidirectional
+        SearchBidirectional SearchBidirectional SearchBidirectional
+        SearchBidirectional SearchBidirectional SearchBidirectional
+        SearchBidirectional Search
+      </>
+    ),
   },
 ];
