57854346523f76eb58367086b4a2a3a09604c702
Viktor <vikt.mag@gmail.com>
2022-09-29 08:56:15 +0200

    Reverted to old implementation of A-Star Search

--- src/Algorithms/Astar.js
+++ src/Algorithms/Astar.js
@@ -1,26 +1,31 @@
+import { PriorityQueue } from "./PriorityQueue";
+
 export function Astar(startNode, goalNode) {
+  var pq = new PriorityQueue();
   var path = [];
   var pathFoundMessage = "CANT FIND PATH";
-  var pathToGoal = [];
   var pathIsFound = false;
+  var pathToGoal = [];
   var algoName = "A-Star Search";
 
-  var openSet = [];
-  var closedSet = [];
+  startNode.distance = 0;
+  pq.enqueue([startNode, 0]);
 
-  openSet.push(startNode);
+  function heuristic(nodeA, nodeB) {
+    let x1 = nodeA.row;
+    let y1 = nodeA.col;
 
-  while (openSet.length > 0) {
-    let minIndex = 0;
-    for (let i = 0; i < openSet.length; i++) {
-      if (openSet[i].f < openSet[minIndex].f) {
-        minIndex = i;
-      }
-    }
+    let x2 = nodeB.row;
+    let y2 = nodeB.col;
 
-    let currentNode = openSet[minIndex];
+    return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+  }
+
+  while (!pq.isEmpty()) {
+    let currentNode = pq.dequeue();
     currentNode.isVisited = true;
     path.push(currentNode);
+    // console.log(`VISITED ${currentNode.row} ${currentNode.col}`);
 
     if (currentNode === goalNode) {
       // console.log(`PATH FOUND ${currentNode.row} ${currentNode.col}`);
@@ -38,42 +43,18 @@ export function Astar(startNode, goalNode) {
       return { path, pathFoundMessage, pathToGoal, pathIsFound, algoName };
     }
 
-    openSet = openSet.filter((node) => node !== currentNode);
-    closedSet.push(currentNode);
-
     let adjacentNodes = currentNode.adjacentNodes;
     for (let i = 0; i < adjacentNodes.length; i++) {
-      if (!closedSet.includes(adjacentNodes[i])) {
-        let gScore = currentNode.g + 1;
-
-        let newPath = false;
-
-        if (openSet.includes(currentNode)) {
-          adjacentNodes[i].g = gScore;
-          newPath = true;
-        } else {
-          adjacentNodes[i].g = gScore;
-          newPath = true;
-          openSet.push(adjacentNodes[i]);
-        }
-
-        if (newPath) {
-          adjacentNodes[i].h = heuristic(adjacentNodes[i], goalNode);
-          adjacentNodes[i].f = adjacentNodes[i].g + adjacentNodes[i].h;
-          adjacentNodes[i].previousNode = currentNode;
+      if (!adjacentNodes[i].isVisited) {
+        let newDist = currentNode.distance + 1;
+        if (newDist < adjacentNodes[i].distance) {
+          adjacentNodes[i].distance = newDist;
+          let priority =
+            adjacentNodes[i].distance + heuristic(adjacentNodes[i], goalNode);
+          pq.enqueue([adjacentNodes[i], priority]);
         }
       }
     }
-  }
-
-  function heuristic(nodeA, nodeB) {
-    let x1 = nodeA.row;
-    let y1 = nodeA.col;
-
-    let x2 = nodeB.row;
-    let y2 = nodeB.col;
-
-    return Math.abs(x1 - x2) + Math.abs(y1 - y2);
   }
 
   return { path, pathFoundMessage, pathIsFound, algoName };
