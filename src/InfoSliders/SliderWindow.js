364e5e1d6b1c9bc3be1767866210fb35ed235e32
Viktor <vikt.mag@gmail.com>
2022-10-05 22:45:35 +0200

    Removed unused useState import in SliderWindow

--- src/InfoSliders/SliderWindow.js
+++ src/InfoSliders/SliderWindow.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { AppContext } from "../Helpers/AppContext";
 import { AlgoInfo } from "./AlgoInfo";
 import { SliderButton } from "./SliderButton";
