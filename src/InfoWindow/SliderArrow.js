c6916a10482c0b94e9b049efd1c2d3964a425c9c
Viktor <vikt.mag@gmail.com>
2022-10-08 20:07:32 +0200

    Added comments and modified SliderArrow.js

--- src/InfoWindow/SliderArrow.js
+++ src/InfoWindow/SliderArrow.js
@@ -6,14 +6,16 @@ export const SliderArrow = ({ changeSlide, direction }) => {
     <button
       onClick={changeSlide}
       className={
+        //Go to new slide based on direction
         direction === "next"
           ? "sliderArrow nextSlideButton"
           : "sliderArrow prevSlideButton"
       }
     >
       <img
+        //Display right arrow for next slide and left arrow for prev slide
         src={direction === "next" ? rightArrow : leftArrow}
-        alt="Right Arrow"
+        alt={direction === "next" ? "Right Arrow" : "Left Arrow"}
       />
     </button>
   );
