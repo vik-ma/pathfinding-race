3a76346703f02d124cadbd80bc9fb302ecc786bb
Viktor <vikt.mag@gmail.com>
2022-10-08 20:02:00 +0200

    Added comments to InfoSlider.js

--- src/InfoWindow/InfoSlider.js
+++ src/InfoWindow/InfoSlider.js
@@ -7,14 +7,17 @@ export const InfoSlider = () => {
   const { setIsInfoRendered, slideIndex, setSlideIndex } =
     useContext(AppContext);
 
+  //Go to slide to the right
   const nextSlideButton = () => {
     setSlideIndex(slideIndex + 1);
   };
 
+  //Go to slide to the left
   const prevSlideButton = () => {
     setSlideIndex(slideIndex - 1);
   };
 
+  //Change which dot is marked at the bottom of InfoWindow
   const moveDot = (index) => {
     setSlideIndex(index);
   };
@@ -23,6 +26,7 @@ export const InfoSlider = () => {
     <div className="popupBackground">
       <div className="popupBackdrop infoBackdrop">
         <div className="infoInner">
+          {/* Show arrow buttons if prev/next slide is available */}
           {slideIndex !== SliderContent.length - 1 ? (
             <SliderArrow changeSlide={nextSlideButton} direction={"next"} />
           ) : null}
@@ -38,6 +42,7 @@ export const InfoSlider = () => {
             </button>
           </div>
 
+          {/* Fill each slide with corresponding index in SliderContent */}
           {SliderContent.map((obj, index) => {
             return (
               <div
@@ -49,6 +54,7 @@ export const InfoSlider = () => {
               </div>
             );
           })}
+          {/* Create clickable dots at bottom of window based on SliderContent elements */}
           <div className="sliderDots">
             {Array.from({ length: SliderContent.length }).map((item, index) => (
               <div
